f53949fdac8064b69e294a92520920474c3d31bc
IsaacShaker <iss73@pitt.edu>
2024-07-26 05:55:48 -0400

    Made heaader and footer componenets more robust. easier to change email address

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -3,6 +3,7 @@ import Socials from "../Socials";
 import Link from "next/link";
 import Button from "../Button";
 
+import data from "../../data/portfolio.json";
 const Footer = ({}) => {
   return (
     <>
@@ -25,8 +26,8 @@ const Footer = ({}) => {
       </div>
       <h1 className="text-sm text-bold mt-2 laptop:mt-10 p-2 laptop:p-0">
         Made With ❤ by{" "}
-        <Link href="http://www.chetanverma.com">
-          <a className="underline underline-offset-1">Chetan Verma</a>
+        <Link href={data.socials[1].link}>
+          <a className="underline underline-offset-1">{data.name}</a>
         </Link>
       </h1>
     </>
